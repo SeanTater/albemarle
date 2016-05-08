8bd0a1d30f7c2539a32ae70489b032c922692fc0
Sean Gallagher <stgallag@gmail.com>
2016-05-08 08:50:27 -0400

    Fixed another problem with remote git

--- travis-push-issue-update.sh
+++ travis-push-issue-update.sh
@@ -2,8 +2,8 @@
 # Update the website
 
 # Use the pages repo
-git config user.name "Sean Gallagher"
-git config user.email "stgallag@gmail.com"
+git config --global user.name "Sean Gallagher"
+git config --global user.email "stgallag@gmail.com"
 git clone --branch gh-pages https://SeanTater@github.com/SeanTater/albemarle.git gh-pages
 cd gh-pages
 
@@ -18,7 +18,7 @@ curl -svSLX GET \
 
 # This monster turns specific lines from the test output into JSON
 # Which has the format [{issue: 1, success:"FAILED"},{issue: 3, success:""}]
-egrep "#\d+" stacktest.log | perl -pe 's/ +#([0-9]+) (.+ (FAILED) \[\d+\]|.+)\n/{issue: \1, success:"\3"},/' | sed -E 's/(.*),$/[\1]/' >_data/stacktest.json
+egrep "#\d+" ../stacktest.log | perl -pe 's/ +#([0-9]+) (.+ (FAILED) \[\d+\]|.+)\n/{issue: \1, success:"\3"},/' | sed -E 's/(.*),$/[\1]/' >_data/stacktest.json
 
 # Put it on the website
 git add _data/issues.json
