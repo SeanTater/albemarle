4a28ec2300d420c046990f67ee4746a6ad67c19a
Sean Gallagher <stgallag@gmail.com>
2016-05-11 16:57:08 -0400

    Move to an ICU based tokenizer

--- src/NLP/Albemarle/Tokens.hs
+++ src/NLP/Albemarle/Tokens.hs
@@ -2,13 +2,19 @@
 module NLP.Albemarle.Tokens
     ( wordTokenize
     ) where
+import ClassyPrelude
 import Data.Text (Text)
+import Control.Applicative ((<$>))
+import qualified Data.Text.ICU as ICU
 import qualified Data.Text as Text
 
 -- | Tokenize text according to the best method available
 wordTokenize :: Text -> [Text]
-wordTokenize = whitespaceTokenize
+wordTokenize = icuTokenize
 
 -- | Tokenize strings idiotically, according to the whitespace
 whitespaceTokenize :: Text -> [Text]
 whitespaceTokenize = Text.words
+
+icuTokenize :: Text -> [Text]
+icuTokenize line = filter (\x -> not $ x==" ") $ ICU.brkBreak <$> ICU.breaks (ICU.breakWord ICU.Current) line
