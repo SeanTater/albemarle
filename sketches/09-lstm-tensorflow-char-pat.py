43a7032754ddc1a2fb065a2775d0e98e290235fa
Sean Gallagher <stgallag@gmail.com>
2016-09-01 07:58:18 -0400

    Multiple layers of gru's

--- sketches/09-lstm-tensorflow-char-pat.py
+++ sketches/09-lstm-tensorflow-char-pat.py
@@ -13,7 +13,7 @@ import numpy as np
 import lzma
 import re
 import random
-import csv
+import sqlite3
 
 # Parameters
 __training_iters = 250000
@@ -26,11 +26,12 @@ __cell_kind = rnn_cell.GRUCell
 #n_input = len(letters) # Unique letters, defined later
 #n_output = len(letters)
 # Patents have about 6300 chars/claim
-__n_steps = 6400 # timesteps - longest sentence
+__n_steps = 200 # timesteps - longest sentence
 __n_hidden = 256 # hidden layer num of features
 
-print('\n\t'.join([
-    "- {}:{}".format(k, v)
+print('\n\t'.join(
+    ["Two layer GRU"]
+    + ["- {}:{}".format(k, v)
     for k, v in vars().items()
     if k.startswith("__") and not k.endswith("__")
 ]))
@@ -49,10 +50,12 @@ one_hots = np.eye(len(letters))
 english = lambda l: _letters_in.find(l) + 1 # unknown = space = first index
 
 
-# Get verses. Pay attention to the memory layout. The vectors are shared.
-def get_book(name, language):
-    print ("Reading {}.".format(name))
-    book = [row[0] for row in csv.reader(lzma.open(name+'.xz', 'rt'))]
+# Get claims. Pay attention to the memory layout. The vectors are shared.
+def get_book(language):
+    print ("Reading patent claims.")
+    with sqlite3.connect("patent-sample.db") as conn:
+        book = [row[0] for row in conn.execute("SELECT claims FROM patent ORDER BY random();").fetchall()]
+
     book = [txt[:__n_steps].ljust(__n_steps) for txt in book]
     booki = np.array([
         [ language(w) for w in l]
@@ -60,11 +63,10 @@ def get_book(name, language):
     book = one_hots[booki]
     lens = np.array([len(l) for l in book], dtype=np.int32)
 
-    print ("Done reading {}.".format(name))
+    print ("Done reading.")
     return (book, booki, lens)
 
-pat = get_book('claims.csv', english)
-
+pat = get_book(english)
 
 all_indices = shuffle(list(range(len(pat[0]))))
 train_indices = all_indices[1000:]
@@ -91,6 +93,12 @@ def transform_block(tensor):
     # Split to get a list of '__n_steps' tensors of shape (__batch_size, n_input)
     return tf.split(0, __n_steps, tensor)
 
+def split_last_axis(tensor):
+    # Reshaping to (__n_steps*__batch_size, n_input)
+    tensor = tf.reshape(tensor, [-1, n_input])
+    # Split to get a list of '__n_steps' tensors of shape (__batch_size, n_input)
+    return tf.split(0, __n_steps, tensor)
+
 def RNN(tensor, lens, name, reuse):
     print ("Building network " + name)
     with tf.variable_scope(name, reuse) as scope:
@@ -107,7 +115,7 @@ def RNN(tensor, lens, name, reuse):
         # Get lstm cell output
         outputs, states = rnn.rnn(cell, tensor, sequence_length=lens, dtype=tf.float32, scope=scope)
         print ("Done building network " + name)
-        # Linear activation, using rnn inner loop last output
+        # Linear activation, using rnn inner loop output for each char
         return tf.batch_matmul(outputs, tf.tile(tf.expand_dims(weights['out'], 0), [__n_steps, 1, 1])) + biases['out']
 
 # tf Graph input
@@ -116,7 +124,9 @@ pat_chars_i = tf.placeholder(tf.int64, [None, __n_steps])
 pat_chars_t = tf.transpose(pat_chars_i, [1,0])
 pat_lens = tf.placeholder(tf.int32, [None])
 
-pat_pred = RNN(transform_block(pat_chars), pat_lens, 'pat', None)
+pat_pred_0 = RNN(transform_block(pat_chars), pat_lens, 'pat0', None)
+pat_pred = RNN(split_last_axis(pat_pred_0), pat_lens, 'pat', None)
+
 print (pat_pred.get_shape(), pat_chars_t.get_shape())
 # Define loss and optimizer
 cost = tf.reduce_mean(
